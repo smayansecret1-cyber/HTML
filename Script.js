3ef60506795213b77e1f2f11ffa2c665ffe5c93e
smayansecret1-cyber <smayansecret1@gmail.com>
2025-12-29 18:54:28 +0530

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,7 @@
+function Myfunction(){
+let userValue = prompt("Enter a value:");
+let num1 = Number(userValue);
+let num2 = Number(userValue);
+let square = num1*num2
+document.getElementById("answer").innerHTML = "the square of "+ num1 + " is " + square
+}
