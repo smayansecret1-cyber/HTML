168679f6da2b5fffa3f5f989d9ea17d02bf35efc
smayansecret1-cyber <smayansecret1@gmail.com>
2026-01-01 18:59:43 +0530

    Add files via upload

---
+++ myfirstnode.js
@@ -0,0 +1,20 @@
+console.log("my name is smayan")
+
+let a=10
+let b=60
+let c=57
+let d=null
+let e;//undefined variable
+console.log("a and b are equal :" + (a==b));
+console.log("");
+console.log("b and c are equal:" + (b==c));
+console.log("\n");
+console.log("value of d is", +d);
+console.log("\n")
+console.log("the value of e is " +e);
+console.log("\n")
+let day=new Date()
+let mins= day.getMinutes();
+var hrs= day.getHours();
+console.log("the current time is" + hrs +":" + mins )
+console.log("\n")
